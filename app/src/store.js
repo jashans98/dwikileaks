a453e9d016085f69cfb487926f2e778dffd73383
James Oldfield <hi@james-oldfield.co>
2017-09-16 10:56:01 -0400

    add root saga to store

--- app/src/store.js
+++ app/src/store.js
@@ -1,22 +1,31 @@
 import { browserHistory } from 'react-router'
 import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
 import { routerMiddleware } from 'react-router-redux'
+
+import rootSaga from './sagas.js'
+
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer'
 
 // Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// instantiate the middleware
+const sagaMiddleware = createSagaMiddleware();
 const routingMiddleware = routerMiddleware(browserHistory)
 
 const store = createStore(
   reducer,
   composeEnhancers(
     applyMiddleware(
-      thunkMiddleware,
-      routingMiddleware
+      routingMiddleware,
+      sagaMiddleware
     )
   )
 )
+
+store.runSaga = sagaMiddleware.run(rootSaga)
+store.asyncReducers = {}
+
 
 export default store
