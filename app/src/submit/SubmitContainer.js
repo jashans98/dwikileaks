1c1abd7b8d4bb4cff00685ff02e1b7bbf46a3423
James Oldfield <hi@james-oldfield.co>
2017-09-16 13:39:54 -0400

    fire web3 from inside submit container

--- app/src/submit/SubmitContainer.js
+++ app/src/submit/SubmitContainer.js
@@ -2,13 +2,33 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
+import Web3 from 'web3'
+
 // SELECTORS
 import { selectSubmitStatus } from './SubmitSelectors'
 
 // ACTIONS
 import { submitDocument } from './SubmitActions'
+import { web3Initialized, web3NoExist } from '../util/web3/web3Actions'
 
 class SubmitContainer extends Component {
+  componentDidMount() {
+    const { web3Initialized, web3NoExist } = this.props
+
+    // Check for MetaMask
+    if (typeof window.web3 !== 'undefined') {
+      const w = new Web3(window.web3.currentProvider)
+
+      console.log('web3 provider found');
+
+      web3Initialized(w)
+    } else {
+      console.log('web3 provider not found');
+      // no metamask? flag for now
+      web3NoExist()
+    }
+  }
+
   render() {
     const { submitDocument } = this.props
 
@@ -23,11 +43,16 @@ class SubmitContainer extends Component {
 SubmitContainer.propTypes = {
   submitStatus: PropTypes.object.isRequired,
   submitDocument: PropTypes.func.isRequired,
+
+  web3Initialized: PropTypes.func.isRequired,
+  web3NoExist: PropTypes.func.isRequired,
 }
 
 export default connect(createStructuredSelector({
   submitStatus: selectSubmitStatus,
 }), {
   submitDocument,
+  web3Initialized,
+  web3NoExist,
 })(SubmitContainer)
 
