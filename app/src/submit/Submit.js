bc03d9be15f53f77bb93bf7ac1cb4f54cedaed24
James Oldfield <hi@james-oldfield.co>
2017-09-16 16:16:05 -0400

    add basic styles for upload button

--- app/src/submit/Submit.js
+++ app/src/submit/Submit.js
@@ -1,14 +1,37 @@
 import React, { PropTypes } from 'react'
 import Dropzone from 'react-dropzone'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+const StyledSubmitText = styled.p`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 10px;
+  box-sizing: border-box;
+`
 
 const Submit = (props) =>
-  <div className="submit">
+  <Wrapper className="submit">
     <Dropzone
       onDrop={props.submitDocument}
     >
-      <p>Upload here...</p>
+      <StyledSubmitText>
+        Click here, or drag file to upload.
+      </StyledSubmitText>
     </Dropzone>
-  </div>
+  </Wrapper>
 
 Submit.propTypes = {
   submitStatus: PropTypes.object.isRequired,
