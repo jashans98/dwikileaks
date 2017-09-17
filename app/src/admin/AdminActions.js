94c05c87f92929db13ad20d280b82260bd3664fa
James Oldfield <hi@james-oldfield.co>
2017-09-17 03:21:28 -0400

    add actions, constants + reducers for downloading docs

--- app/src/admin/AdminActions.js
+++ app/src/admin/AdminActions.js
@@ -1,11 +1,18 @@
 import {
   GET_LEAKS,
+  DOWNLOAD_DOCUMENT,
 } from './AdminConstants'
 
 const getLeaks = () => ({
   type: GET_LEAKS,
 })
 
+const downloadDocument = hash => ({
+  type: DOWNLOAD_DOCUMENT,
+  hash,
+})
+
 export {
   getLeaks,
+  downloadDocument,
 }
