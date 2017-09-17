1cf0f39a531e0d80fe2048cae121d26d621eb038
James Oldfield <hi@james-oldfield.co>
2017-09-17 12:17:39 -0400

    add styles for admin view

--- app/src/admin/Admin.js
+++ app/src/admin/Admin.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { Card, Text } from 'rebass'
+import { Card, Text, Heading } from 'rebass'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  margin-top: 100px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 const Admin = props =>
-  <div>
+  <Wrapper>
+    <Heading
+      style={{
+        marginBottom: '50px'
+      }}
+    >Here are your latest file submissions</Heading>
+
     {props.leaks.length && props.leaks.map((leak, i) =>
       <Card
+        style={{
+          padding: '30px 5px',
+          borderRadius: '10px',
+          background: '#eee',
+          border: 'solid 2px #bcbcbc',
+          marginBottom: '15px',
+        }}
         onClick={() => props.downloadDocument(leak)}
         key={i}
         px={3}
@@ -13,15 +37,19 @@ const Admin = props =>
         mb={3}
         width={[ 1, 1/2 ]}
         fontSize={[ 4, 5, 6 ]}
-        color='white'
-        bg='blue'
+        color='black'
+        bg='grey'
       >
-        <Text>
+        <Text
+          style={{
+            fontSize: '15px',
+          }}
+        >
           {leak}
         </Text>
       </Card>
     )}
-  </div>
+  </Wrapper>
 
 Admin.propTypes = {}
 
